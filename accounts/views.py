940e1332afee1d92f4e90bb230631a33f7289c4c
Ubuntu <ubuntu@ip-172-31-40-250.eu-west-1.compute.internal>
2020-05-04 13:34:16 +0000

    implemented an edit profile functinality in the accounts app

---
+++ accounts/views.py
@@ -0,0 +1,89 @@
+from django.shortcuts import render, redirect, HttpResponseRedirect, reverse
+from django.contrib import messages, auth
+from .forms import UserLoginForm, UserRegistrationForm, UserUpdateForm
+from django.template.context_processors import csrf
+from django.contrib.auth.decorators import login_required
+
+
+
+# Create your views here.
+
+def logout(request):
+    """A view that logs the user out and redirects back to the index page"""
+    auth.logout(request)
+    messages.success(request, 'You have successfully logged out')
+    return redirect(reverse('index'))
+
+
+def login(request):
+    """
+    A view that manages the login form.
+    The firs if statement prevent the access of the login page after the 
+    user is logged in.
+    """
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
+
+    if request.method == 'POST':
+        user_form = UserLoginForm(request.POST)
+        if user_form.is_valid():
+            user = auth.authenticate(request.POST['username_or_email'],
+                                     password=request.POST['password'])
+
+            if user:
+                auth.login(request, user)
+                messages.success(request, "You have successfully logged in")
+
+                if request.GET and request.GET['next'] !='':
+                    next = request.GET['next']
+                    return HttpResponseRedirect(next)
+                else:
+                    return redirect(reverse('index'))
+            else:
+                user_form.add_error(None, "Your username or password are incorrect")
+    else:
+        user_form = UserLoginForm()
+
+    args = {'user_form': user_form, 'next': request.GET.get('next', '')}
+    return render(request, 'login.html', args)
+
+@login_required
+def profile(request):
+    """A view that displays the profile page of a logged in user"""
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        
+        if user_form.is_valid:
+            user_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('login')
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+    
+    args = {'user_form': user_form}
+    return render(request, 'profile.html', args)
+
+
+def register(request):
+    """A view that manages the registration form"""
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            user_form.save()
+
+            user = auth.authenticate(request.POST.get('email'),
+                                     password=request.POST.get('password1'))
+
+            if user:
+                auth.login(request, user)
+                messages.success(request, "You have successfully registered")
+                return redirect(reverse('index'))
+
+            else:
+                messages.error(request, "unable to log you in at this time!")
+    else:
+        user_form = UserRegistrationForm()
+
+    args = {'user_form': user_form}
+    return render(request, 'register.html', args)
+
