f9ba864ceec59c1ed15978ff86b8928753b24ce9
Ubuntu <ubuntu@ip-172-31-40-250.eu-west-1.compute.internal>
2020-05-09 14:27:10 +0000

    more products added to the database and pagination system implemented in category.html with an extended functionality of the category view

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.views.generic.list import ListView
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Product
 
 def get_product(request, id):
@@ -13,8 +14,19 @@ def get_product(request, id):
     
 def category(request, type):
     """
-    A view that return the specified category
+    A view that return the specified category of products
+    and implement a pagination system for the pages.
     """
     products = Product.objects.filter(product_type=type)
+    page = request.GET.get('page', 1)
+    
+    paginator = Paginator(products, 6)
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+    
     args = {'products': products}
     return render(request, 'category.html', args)
